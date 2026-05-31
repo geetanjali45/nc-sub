eb85f0406d3bfecaffd29c2c55748a144f16ef26
Geetanjali <geetanjali.reach@gmail.com>
2026-05-31 14:09:41 +0530

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void quickSort(int[] nums, int low, int high) {
+        if (low < high) {
+            int p = partition(nums, low, high);
+            quickSort(nums, low, p - 1);
+            quickSort(nums, p + 1, high);
+        }
+    }
+    public int partition(int[] nums, int low, int high) {
+        int pivot = nums[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (nums[j] < pivot) {
+                i++;
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        int temp = nums[i + 1];
+        nums[i + 1] = nums[high];
+        nums[high] = temp;
+        return i + 1;
+    }
+    public int[] sortArray(int[] nums) {
+        int n = nums.length;
+        quickSort(nums, 0, n - 1);
+        return nums;
+    }
+}
