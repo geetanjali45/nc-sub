02689139fb756b6d82d6891e93e24141f66f5015
geetanjali <geetanjali.reach@gmail.com>
2026-06-02 13:18:15 +0530

    Add: first-missing-positive - submission-15

---
+++ Data Structures & Algorithms/first-missing-positive/submission-15.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int sz = nums.length, i = 0;
+        Arrays.sort(nums);
+        while (i < sz && nums[i] <= 0) i++;
+        System.out.println(i);
+        if (i == sz || nums[i] > 1 || nums[sz - 1] <= 0)
+            return 1;
+
+        for (int j = Math.max(i, 1); j < sz; j++) {
+            if (nums[j] == nums[j - 1] + 1 || nums[j] == nums[j - 1] || nums[j] == 1)
+                continue;
+            else if (nums[j] > nums[j - 1] + 1)
+                return nums[j - 1] + 1;
+        }
+        if (nums[sz - 1] > 0)
+            return nums[sz - 1] + 1;
+        return -1;
+    }
+}
